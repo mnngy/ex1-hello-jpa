ea8befcda136647a5b3afd6e16c4cf503e5d899c
InGyu Min <59257197+mnngy@users.noreply.github.com>
2022-01-16 17:25:47 +0900

    영속성 컨텍스트 1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,2 +1,37 @@
-package hellojpa;public class JpaMain {
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJpa");
+
+            // 영속
+            System.out.println("==== BEFORE ====");
+            em.persist(member);
+            System.out.println("==== START ====");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
 }
