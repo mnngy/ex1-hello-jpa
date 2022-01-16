4c4a446778ebf4af23752f3f3cb207bb289944cf
InGyu Min <59257197+mnngy@users.noreply.github.com>
2022-01-16 18:00:58 +0900

    영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,45 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /*
             // 비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJpa");
 
             // 영속
             System.out.println("==== BEFORE ====");
             em.persist(member);
             System.out.println("==== START ====");
+
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+             */
+
+            /*
+            Member member1 = em.find(Member.class, 101L);
+            Member member2 = em.find(Member.class, 101L);
+
+            System.out.println(member1 == member2);
+             */
+
+            /*
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("============");
+             */
+
+            /*
+            Member member = em.find(Member.class, 150L);
+            member.setName("asdf");
+
+            System.out.println("============");
+             */
 
             tx.commit();
         } catch (Exception e) {
